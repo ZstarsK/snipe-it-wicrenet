56044a30a117f0a03b125d108e8ab90f027a9cae
Zhixin Ke <1262937829@qq.com>
2025-11-19 12:21:25 +0800

    Delete the footer

--- resources/views/layouts/basic.blade.php
+++ resources/views/layouts/basic.blade.php
@@ -55,12 +55,6 @@
   <!-- Content -->
   @yield('content')
 
-    <div class="text-center" style="padding-top: 100px;">
-        @if (($snipeSettings) && ($snipeSettings->privacy_policy_link!=''))
-        <a target="_blank" rel="noopener" href="{{  $snipeSettings->privacy_policy_link }}" target="_new">{{ trans('admin/settings/general.privacy_policy') }}</a>
-    @endif
-    </div>
-
     {{-- Javascript files --}}
     <script src="{{ url(mix('js/dist/all.js')) }}" nonce="{{ csrf_token() }}"></script>
 
